5891ce950b9ba4a91d0aa4a1c7e34e2b2e690600
Gennady Zaitcev <hikka.down@yandex.ru>
2021-04-08 23:29:54 +0300

    Переписал валидацию форм

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,92 +1,4 @@
-// элементы которые нужны
-// popup__container popup__container_type_edit-form - имя формы
-// popup__input popup__input_type_name - имя инпута 1 
-// popup__error_type_name-profile - имя ошибки для инпута 1
-// popup__input popup__input_type_job - имя инпута 2
-// popup__error popup__error_type_job-profile - имя ошибки для инпута 2
-
-// popup__container popup__container_type_add-form - имя формы 
-// popup__input popup__input_type_name-card - имя инпута 1
-// popup__error popup__error_type_name-card - имя ошибки для инпута 1
-// popup__input popup__input_type_url-img - имя инпута 2
-// popup__error popup__error_type_url-image - имя ошибки инпута 2
-// 
-// popup__submit - кнопка сохранить
-// popup__submit_disabled - неактивное сост для сохранить
-// popup__error_visible - выводит ошибку под инпутом
-// popup__input_type_error - показывает что поле недопустимое
-
-
-// function enableValidation(sett) {
-//   const forms = document.querySelectorAll(sett.formSelector);
-//   // это форма с редактированием профиля
-//   forms[0].addEventListener('keyup', (e) => {
-//     const inputs = forms[0].querySelectorAll(sett.inputSelector);
-//     const errorsContent = forms[0].querySelectorAll(sett.errorSelector);
-//     const submitButton = forms[0].querySelector(sett.submitButtonSelector);
-//     let validFlag = true;
-//     if(inputs[0].value.length < 2 || inputs[0].value.length > 40) {
-//       validFlag = false ;
-//       errorsContent[0].classList.add(sett.errorVisible);
-//       inputs[0].classList.add(sett.inputErrorClass);
-//     } else {
-//       errorsContent[0].classList.remove(sett.errorVisible);
-//       inputs[0].classList.remove(sett.inputErrorClass);
-//       validFlag = validFlag ? true : false;
-//     }
-//     if(inputs[1].value.length < 2 || inputs[1].value.length > 200) {
-//       validFlag = false ;
-//       errorsContent[1].classList.add(sett.errorVisible);
-//       inputs[1].classList.add(sett.inputErrorClass);
-//     } else {
-//       errorsContent[1].classList.remove(sett.errorVisible);
-//       inputs[1].classList.remove(sett.inputErrorClass);
-//       validFlag = validFlag ? true : false;
-//     }
-//     if(!validFlag) {
-//       submitButton.classList.add(sett.inactiveButtonClass);
-//       submitButton.disabled = true;
-//     } else {
-//       submitButton.classList.remove(sett.inactiveButtonClass);
-//       submitButton.disabled = false;
-//     }
-//   });
-//   // тут форма с добавлением карточки
-//   forms[1].addEventListener('input', () => {
-//     const inputs = forms[1].querySelectorAll(sett.inputSelector);
-//     const errorsContent = forms[1].querySelectorAll(sett.errorSelector);
-//     const submitButton = forms[1].querySelector(sett.submitButtonSelector);
-//     let validFlag = true;
-//     if(inputs[0].value.length < 1 || inputs[0].value.length > 30) {
-//       validFlag = false;
-//       errorsContent[0].classList.add(sett.errorVisible);
-//       inputs[0].classList.add(sett.inputErrorClass);
-//     } else {
-//       errorsContent[0].classList.remove(sett.errorVisible);
-//       inputs[0].classList.remove(sett.inputErrorClass);
-//       validFlag = validFlag ? true : false;
-//     }
-//     if(inputs[1].validity.valid && inputs[1].value.length > 0) {
-//       validFlag = validFlag ? true : false;
-//       errorsContent[1].classList.remove(sett.errorVisible);
-//       inputs[1].classList.remove(sett.inputErrorClass);
-//     } else {
-//       errorsContent[1].classList.add(sett.errorVisible);
-//       inputs[1].classList.add(sett.inputErrorClass);
-//       validFlag = false 
-//     }
-//     // делаю кнопку отправки не активной или активной
-//     if(!validFlag) {
-//       submitButton.classList.add(sett.inactiveButtonClass);
-//       submitButton.disabled = true;
-//     } else {
-//       submitButton.classList.remove(sett.inactiveButtonClass);
-//       submitButton.disabled = false;
-//     }
-//   });
-// }
-
-let settings = {
+const settings = {
   formSelector: 'form.popup__container',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__submit',
@@ -96,56 +8,65 @@ let settings = {
   errorVisible: 'popup__error_visible'
 }
 
-function enableValidation(objData) {
-  const formList = Array.from(document.querySelectorAll(objData.formSelector));
-  function isValid(formElement, inputElement) {
-    if(!inputElement.validity.valid) {
-      // showInputError теперь получает параметром форму, в которой
-      // находится проверяемое поле, и само это поле
-      showInputError(formElement, inputElement, inputElement.validationMessage);
-    } else {
-      // hideInputError теперь получает параметром форму, в которой
-      // находится проверяемое поле, и само это поле
-      hideInputError(formElement, inputElement);
-    }
-  }
-  
-  function showInputError(formElement, inputElement, errorMessage) {
-    // const submitBtn = formElement.querySelector(objData.submitButtonSelector);
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.add(objData.inputErrorClass);
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add(objData.errorVisible);
-    console.log(formElement);
-    // submitBtn.classList.add(objData.inactiveButtonClass);
-    // submitBtn.setAttribute('disabled', true);
-  }
-  
-  function hideInputError(formElement, inputElement) {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    // const submitBtn = formElement.querySelector(objData.submitButtonSelector);
-    inputElement.classList.remove(objData.inputErrorClass);
-    errorElement.classList.remove(objData.errorVisible);
-    errorElement.textContent = '';
-    // submitBtn.classList.remove(objData.inactiveButtonClass);
-    // submitBtn.removeAttribute('disabled', true);
-  }
-  
-  function setEventListeners(formElement) {
-    const inputList = Array.from(formElement.querySelectorAll(objData.inputSelector));
-    inputList.forEach((inputElement) => {
-      inputElement.addEventListener('input', () => {
-        isValid(formElement, inputElement);
-      });
-    });
-  }
-
+function enableValidation(formData) {
+  const formList = Array.from(document.querySelectorAll(formData.formSelector));
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', (e) => {
       e.preventDefault();
     });
     setEventListeners(formElement);
   });
+
+  function isValid(formElement, inputElement) {
+    if(!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  }
+
+  function hasInvalidInput(inputList) {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+  
+  function toggleButtonState(inputList, buttonElement) {
+    if(hasInvalidInput(inputList)) {
+      buttonElement.classList.add(formData.inactiveButtonClass);
+      buttonElement.setAttribute('disabled', true);
+    } else {
+      buttonElement.classList.remove(formData.inactiveButtonClass);
+      buttonElement.removeAttribute('disabled', true);
+    }
+  }
+
+  function showInputError(formElement, inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(formData.inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(formData.errorVisible);
+  }
+  
+  function hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(formData.inputErrorClass);
+    errorElement.classList.remove(formData.errorVisible);
+    errorElement.textContent = '';
+  }
+  
+  function setEventListeners(formElement) {
+    const inputList = Array.from(formElement.querySelectorAll(formData.inputSelector));
+    const buttonElement = formElement.querySelector('.popup__submit');
+    toggleButtonState(inputList, buttonElement);
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        isValid(formElement, inputElement);
+        toggleButtonState(inputList, buttonElement);
+      });
+    });
+  }
+
 }
 
 enableValidation(settings);
