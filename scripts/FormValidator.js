61de5512e44d5aeea88597cf9907c3fb1376aaf9
Gennady Zaitcev <hikka.down@yandex.ru>
2021-04-26 16:58:22 +0300

    Поменял form на обращение через объект

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -9,22 +9,22 @@ class FormValidator {
     this._errorVisible = formData.errorVisible;
   }
 
-  _setEventListeners(form) {
-    const inputList = Array.from(form.querySelectorAll(this._inputSelector));
-    const buttonElement = form.querySelector(this._submitButtonSelector);
+  _setEventListeners() {
+    const inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
+    const buttonElement = this._formElement.querySelector(this._submitButtonSelector);
     inputList.forEach((inputElement) => {
       inputElement.addEventListener('input', () => {
-        this._isValid(form, inputElement);
+        this._isValid(inputElement);
         this._toggleButtonState(inputList, buttonElement);
       });
     });
   }
 
-  _isValid(formElement, inputElement) {
+  _isValid(inputElement) {
     if(!inputElement.validity.valid) {
-      this._showInputError(formElement, inputElement, inputElement.validationMessage);
+      this._showInputError(inputElement, inputElement.validationMessage);
     } else {
-      this._hideInputError(formElement, inputElement);
+      this._hideInputError(inputElement);
     }
   }
 
@@ -44,23 +44,22 @@ class FormValidator {
     }
   }
 
-  _showInputError(formElement, inputElement, errorMessage) {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  _showInputError( inputElement, errorMessage) {
+    const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
     inputElement.classList.add(this._inputErrorClass);
     errorElement.textContent = errorMessage;
     errorElement.classList.add(this._errorVisible);
   }
 
-  _hideInputError(formElement, inputElement) {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  _hideInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
     inputElement.classList.remove(this._inputErrorClass);
     errorElement.classList.remove(this._errorVisible);
     errorElement.textContent = '';
   }
 
   enableValidation() {
-    const form = this._formElement;
-    form.addEventListener('submit', (e) => {
+    this._formElement.addEventListener('submit', (e) => {
       e.preventDefault();
     });
     this._setEventListeners(this._formElement);
